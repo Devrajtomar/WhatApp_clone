cf1856be388513d066b0385896c1187e6b5e80b6
unknown <devrajtomar120@gmail.com>
2023-08-20 07:02:55 +0530

    deleted console logs

--- src/components/main/CallsSpace.jsx
+++ src/components/main/CallsSpace.jsx
@@ -2,17 +2,18 @@ import JitsiContainer from "@/utils/Calls";
 import { useEffect } from "react";
 
 const CallsSpace = () => {
-  const frame = document.getElementById("jitsiConferenceFrame0");
-  if (frame !== null) {
-    const container = document.getElementById("CallContainer");
-    container.appendChild(frame);
-  }
+  // const frame = document.getElementById("jitsiConferenceFrame0");
+  // if (frame !== null) {
+  //const container = document.getElementById("CallContainer");
+  //   container.appendChild(frame);
+  // }
 
   return (
-    <div className="page">
-      <JitsiContainer />
-      <div id="CallContainer"></div>
-    </div>
+    //    <div className="page">
+    // <JitsiContainer />
+    //      <div id="CallContainer"></div>
+    //    </div>
+    <div className="page">Calls</div>
   );
 };
 
